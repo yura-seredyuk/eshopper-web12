d5d9453b4cad8620d788afe58ac7898184a628bd
Yurii Serediuk <yurii.seredyuk@gmail.com>
2022-09-22 21:32:27 +0300

    Added filters attributes

--- shop/views.py
+++ shop/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, ProductCategory
+from .models import Product, ProductCategory, Country
 
 def homepage(request):
-    return render(request, 'pages/index.html')
+    categories = ProductCategory.objects.all()
+   
+    return render(request, 'pages/index.html', {'categories':categories})
 
 
 def shop_page(request, category_slug=None):
     categories = ProductCategory.objects.all()
+    countries = Country.objects.all() 
     products = Product.objects.all()
     category = None
-    
+
+    print(Product.objects.all().count())
+
     if category_slug:
         category = ProductCategory.objects.filter(slug=category_slug)[0]
         products = Product.objects.filter(product_category=category).distinct()
     return render(request, 'shop/shop.html', {'products':products,
                                             'categories':categories,
+                                            'countries':countries,
                                             'selected_category': category
                                             })
 
 
 def product_detail_page(request, pk):
+    categories = ProductCategory.objects.all()
+
     product = get_object_or_404(Product, pk=pk)
-    return render(request, 'shop/detail.html', {'product':product})
+    return render(request, 'shop/detail.html', {'product':product,
+                                            'categories':categories})
