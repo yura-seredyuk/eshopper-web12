d250046a8a137304ef7eb86bcaf0a401de89933e
Yurii Serediuk <yurii.seredyuk@gmail.com>
2022-09-27 21:21:25 +0300

    Added category filter

--- shop/views.py
+++ shop/views.py
@@ -11,27 +11,50 @@ def shop_page(request, category_slug=None):
     categories = ProductCategory.objects.all()
     countries = Country.objects.all() 
     products = Product.objects.all()
-    category = None
+    selected_category = None
 
     categories_count = {}
     countries_count = {}
     for country in countries:
         countries_count.update({country.country_name:Product.objects.filter(country=country.id).count()})
+    
+    selected_categories = []
+    if request.method == 'POST':
+        print(request.POST.keys())
+        for item in request.POST.keys():
+            if 'category' in item:
+                if item == 'category-all': 
+                    category_slug = selected_category = None
+                    request.path_info = '/shop/'
+                    break
+                selected_categories.append(item[9:])
+
+    print(1, selected_categories, category_slug)
 
     for category in categories:
         categories_count.update({category.category_name:Product.objects.filter(product_category=category.id).count()})
-    print(categories_count)
-    if category_slug:
-        category = ProductCategory.objects.filter(slug=category_slug)[0]
-        products = Product.objects.filter(product_category=category).distinct()
+
+    if len(selected_categories) > 0:
+        s_categoryes = ProductCategory.objects.filter(category_name__in=selected_categories)
+        products = Product.objects.filter(product_category__in=s_categoryes).distinct()
+        category_slug = selected_category = None
+        request.path_info = '/shop/'
+    elif category_slug:
+        selected_category = ProductCategory.objects.filter(slug=category_slug)[0]
+        products = Product.objects.filter(product_category=selected_category).distinct()
+    print(2, selected_categories, category_slug)
+    print(request.path)
+
     return render(request, 'shop/shop.html', {'products':products,
-                                            # 'categories':categories,
+                                            'categories':categories,
                                             # 'countries':countries,
-                                            'selected_category': category,
+                                            'selected_category': selected_category,
                                             'countries_count': countries_count,
                                             'categories_count': categories_count
                                             })
 
+def categories_filter(request):
+    pass 
 
 def product_detail_page(request, pk):
     categories = ProductCategory.objects.all()
