e234c8d5e47c227c5b725beaa399b77fe200b57e
Yurii Serediuk <yurii.seredyuk@gmail.com>
2022-10-17 19:47:44 +0300

    added profile page

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
+from shop.models import Profile, Country
 
 
 def profile(request):
-   
-    return render(request, 'user_profile/profile.html')
+    user = ''
+    profile = ''
+    address = ''
+    city = ''
+    country = ''
+
+    user = request.user
+    if user.is_active:
+        profile = Profile.objects.filter(user=user).first()
+        address = profile.address
+        city = address.city
+        country = Country.objects.filter(pk=city.country.id).first()
+
+    return render(request, 'user_profile/profile.html', {"user":user,
+                                                        "address":address,
+                                                        "city":city,
+                                                        "profile":profile,
+                                                        "country":country})
 
